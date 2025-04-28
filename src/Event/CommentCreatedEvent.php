81bfc77eb41a59b55b84740958076268319474b9
Juan Vargas <66626747+v4rgas@users.noreply.github.com>
2025-04-28 13:28:31 -0400

    Update CommentCreatedEvent.php

--- src/Event/CommentCreatedEvent.php
+++ src/Event/CommentCreatedEvent.php
@@ -14,15 +14,3 @@ namespace App\Event;
 use App\Entity\Comment;
 use Symfony\Contracts\EventDispatcher\Event;
 
-final class CommentCreatedEvent extends Event
-{
-    public function __construct(
-        protected Comment $comment,
-    ) {
-    }
-
-    public function getComment(): Comment
-    {
-        return $this->comment;
-    }
-}
